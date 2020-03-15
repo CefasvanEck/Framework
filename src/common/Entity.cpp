b89a231e0074c3e9db941136ad862c39cdffa6cf
CefasvanEck <50441721+CefasvanEck@users.noreply.github.com>
2020-03-15 14:42:56 +0100

    V1.1.2
    Add Temporary fix in Entity to fix the scale not set properly
    Add comment about scale in Entity.cpp
    Add Note in Entity.cpp for why the scale is a lower number

--- src/common/Entity.cpp
+++ src/common/Entity.cpp
@@ -9,7 +9,14 @@
 Entity::Entity(std::string texture, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale)
 {
 	this->sprite = new Sprite(texture);
-	//this->setScale(glm::vec3(1,1,1));
+
+	/**
+	 * For some reason the scale wil have a negative value while it is set in the constructor(see Entity.h)
+	 Note: why is scale -431602080.000000 and not 1? (glm::vec3 scale = glm::vec3(0, 0, 0) in constructor Entity.h)
+	 For some reason the scale is uninitialized?
+	 */
+	Main::getInstance().getConsole()->println(this->getScale().x);
+	this->setScale(glm::vec3(1,1,1));
 	Main::getInstance().getConsole()->println(this->getScale().x);
 }
 
