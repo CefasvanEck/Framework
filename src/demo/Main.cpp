ddb7cfa90afc0cba16224c40a600893431f7bbb8
CefasvanEck <50441721+CefasvanEck@users.noreply.github.com>
2020-03-16 12:33:40 +0100

    V1.1.7
    Add in Main start function an Entity without a texture path, position, rotation and scale

--- src/demo/Main.cpp
+++ src/demo/Main.cpp
@@ -22,11 +22,11 @@ Main::~Main()
  */
 void Main::start()
 {
+    this->addEntity(new Entity());
     this->addEntity(new Entity("assets/rgba.tga", glm::vec3(100, 100, 0), glm::vec3(0, 0, 0), glm::vec3(1, 1, 1)));
     this->addEntity(new Entity("assets/kingkong.tga", glm::vec3(100, 200, 0), glm::vec3(0, 0, 0), glm::vec3(1, 1, 1)));
     this->addEntity(new Entity("assets/pencils.tga", glm::vec3(200, 300, 0), glm::vec3(0, 0, 0), glm::vec3(1, 1, 1)));
     this->addEntity(new Entity("assets/pencils.tga", glm::vec3(200, 500, 0), glm::vec3(0, 0, 0), glm::vec3(1, 1, 1)));
-
 }
 
 /**
@@ -37,7 +37,6 @@ void Main::fixedRunning()
 {
 
 }
-
 
 /**
  * Called when running the game
